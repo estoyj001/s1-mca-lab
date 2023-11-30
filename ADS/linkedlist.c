f508b9b733ea1c2da384c37f24cd6c332cf6820d
Nelwni <ajunaidkb14@gmail.com>
2023-11-30 15:21:43 +0530

    Linked List Segmentation Fault

--- ADS/linkedlist.c
+++ ADS/linkedlist.c
@@ -7,15 +7,22 @@ struct node{
 }*head = NULL;
 
 void create(int val){
-	head->data = val;
-	head -> next = NULL;
+	if(head==NULL){
+		head = malloc(sizeof(struct node));
+		head->data = val;
+		head->next=NULL;
+	}
+	else{
+		
+	}
+	
 }
 
 void insertbeg(int val){
 	if(head==NULL){
 		create(val);
 	}
-	struct node* newnode = malloc(sizeof(struct node*));
+	struct node* newnode = malloc(sizeof(struct node));
 	newnode->next = head;
 	newnode->data = val;
 	head = newnode;
@@ -23,10 +30,14 @@ void insertbeg(int val){
 }
 
 void append(int val){
-	struct node* temp = malloc(sizeof(struct node*));
+	struct node* temp = head;
 	struct node* newnode = malloc(sizeof(struct node*));
-	newnode->next = head;
+	newnode->next = NULL;
 	newnode->data = val;
+	if (head == NULL) {
+        head = newnode;
+        return;
+    }
 	while(temp->next!=NULL){
 		temp = temp->next;
 	
@@ -35,20 +46,64 @@ void append(int val){
 	temp->next = newnode;
 }
 
+void insertpos(int val,int pos){
+	struct node* temp = head;
+	for(int i=1;i<=pos;i++){
+		temp=temp->next;	
+	}
+	struct node* newnode = malloc(sizeof(struct node));
+	newnode->next = temp->next;
+	temp->next=newnode;
+	
+	
+}
+
 void printll(){
 	printf("Contents of your linked list are : ");
-	struct node* temp = malloc(sizeof(struct node*));
-	while(temp->next!=NULL){
+	struct node* temp = head;
+	while(temp!=NULL){
 		printf(" %d ",temp->data);
+		temp = temp->next;
 	}
 }
 
 
 int main(){
-	create(5);
-	append(10);
-	append(15);
-	printll();
-	return 0;
+	int c,val,pos;
+	printf("Choose An Option :\n1.Create A Linked List\n2.Insert At Beginning\n3.Insert At End (Appending)\n4.Insert At Desired Position\n5.Print Your Linked List\n");
+	while(1){
+	
+	scanf("%d",&c);
+	
+	switch(c){
+		case 1 : printf("\nEnter Value For Your First Element : ");
+		scanf("%d",&val);
+		create(val);
+		break;
+		
+		case 2 : printf("\nEnter Value To Be Inserted At Beginning : ");
+		scanf("%d",&val);
+		insertbeg(val);
+		break;
+		
+		case 3 : printf("\nEnter Value To Append : ");
+		scanf("%d",&val);
+		append(val);
+		break;
+		
+		case 4 : printf("\nEnter Value and Position to Insert : ");
+		scanf("%d %d",&val,&pos);
+		insertpos(val,pos);
+		break;
+		
+		case 5 : 
+		printll();
+		break;
+		
+		default : printf("Invalid Input, Try Again!");
+		return 0;
+	}
+	}
+	
 	
 }
